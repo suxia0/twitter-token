ea35f52dc0f85967dcdb97f30228c61cace7a16c
suxia0 <150435708+suxia0@users.noreply.github.com>
2025-01-11 21:35:43 +0800

    Update index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Twitter Token Management App!</h1>
+      <p>Please log in to manage your Twitter tokens.</p>
+    </div>
+  );
+};
+
+export default HomePage;
